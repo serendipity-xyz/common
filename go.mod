4ef773ae25dfda527c099bcde75d62a598136560
David Wallach <davidwallach2@gmail.com>
2022-09-25 19:18:38 -0400

    sqs-working

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/serendipity-xyz/core
 go 1.19
 
 require (
+	github.com/aws/aws-sdk-go v1.44.105 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- types/types.go
+++ types/types.go
@@ -1,8 +1,32 @@
 package types
 
+import "fmt"
+
 type Logger interface {
 	Debug(string, ...interface{})
 	Info(string, ...interface{})
 	Warn(string, ...interface{})
 	Error(string, ...interface{})
+}
+
+type StdOutLogger struct{}
+
+func (l StdOutLogger) Debug(s string, a ...interface{}) {
+	fmt.Printf(s, a...)
+	fmt.Println()
+}
+
+func (l StdOutLogger) Info(s string, a ...interface{}) {
+	fmt.Printf(s, a...)
+	fmt.Println()
+}
+
+func (l StdOutLogger) Warn(s string, a ...interface{}) {
+	fmt.Printf(s, a...)
+	fmt.Println()
+}
+
+func (l StdOutLogger) Error(s string, a ...interface{}) {
+	fmt.Printf(s, a...)
+	fmt.Println()
 }
